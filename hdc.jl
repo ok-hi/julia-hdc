7d40ff8ba8bb2da5e8513f0f8aa990012b73d88c
Sherm <brendclav@gmail.com>
2021-01-22 16:52:50 -0800

    put init funcs inside constructors

--- hdc.jl
+++ hdc.jl
@@ -9,25 +9,18 @@ vec_len = 10000
 # don't have to call rand(0:1, vec_len) all the time
 struct hdc_vector
     value::Array{Bool,1}
+    hdc_vector(value = rand(0:1, vec_len)) = new(value)
 end
 
 struct item_memory
     mem::Dict{String,hdc_vector}
-end
-
-function init_mem()
-    return item_memory(Dict{String,hdc_vector}())
+    item_memory() = new(Dict{String,hdc_vector}())
 end
 
 function push(mem::item_memory, name = randstring(rand_name_len))
-    new_vec = init_vec()
+    new_vec = hdc_vector()
     push!(mem.mem, name => new_vec)
     return new_vec
-end
-
-# init_vec is just a wrapper for initialization
-function init_vec(value = rand(0:1, vec_len))
-    return hdc_vector(value)
 end
 
 # bsc bundling operation (addition with a threshold of 1)
@@ -35,13 +28,13 @@ function +(a::hdc_vector, b::hdc_vector)
     x = a.value + b.value
     x[x .== 1] = rand(0:1, length(x[x .== 1])) # replace all ones in x with either a 1 or a 0 (random)
     x[x .== 2] = ones(Int64, length(x[x .== 2])) # replace all twos with a 1
-    return init_vec(x)
+    return hdc_vector(x)
 end
 
 # bsc binding operation
 function *(a::hdc_vector, b::hdc_vector)
     new_val = a.value .⊻ b.value
-    return init_vec(new_val)
+    return hdc_vector(new_val)
 end
 
 # hamming distance
